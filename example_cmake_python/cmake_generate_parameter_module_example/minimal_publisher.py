2a880feef0c52eade13584b289563c9cdd55dcf1
Tyler Weaver <maybe@tylerjw.dev>
2024-01-12 11:14:26 -0700

    Rebase from 'upstream'

--- example_cmake_python/cmake_generate_parameter_module_example/minimal_publisher.py
+++ example_cmake_python/cmake_generate_parameter_module_example/minimal_publisher.py
@@ -27,10 +27,49 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+import rclpy
+import rclpy.node
 
-def no_args_validator(param):
-    return ''
+from cmake_generate_parameter_module_example.admittance_parameters import (
+    admittance_controller,
+)
 
 
-def validate_double_array_custom_func(param, arg1, arg2):
-    return ''
+class MinimalParam(rclpy.node.Node):
+    def __init__(self):
+        super().__init__('admittance_controller')
+        self.timer = self.create_timer(1, self.timer_callback)
+
+        self.param_listener = admittance_controller.ParamListener(self)
+        self.params = self.param_listener.get_params()
+        self.get_logger().info(
+            "Initial control frame parameter is: '%s'" % self.params.control.frame.id
+        )
+        self.get_logger().info("fixed string is: '%s'" % self.params.fixed_string)
+
+        self.get_logger().info(
+            "Original joints parameter is: '%s'" % str(self.params.joints)
+        )
+        for d in self.params.fixed_array:
+            self.get_logger().info("value: '%s'" % str(d))
+
+    def timer_callback(self):
+        if self.param_listener.is_old(self.params):
+            self.param_listener.refresh_dynamic_parameters()
+            self.params = self.param_listener.get_params()
+            self.get_logger().info(
+                "New control frame parameter is: '%s'" % self.params.control.frame.id
+            )
+            self.get_logger().info("fixed string is: '%s'" % self.params.fixed_string)
+            for d in self.params.fixed_array:
+                self.get_logger().info("value: '%s'" % str(d))
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MinimalParam()
+    rclpy.spin(node)
+
+
+if __name__ == '__main__':
+    main()
