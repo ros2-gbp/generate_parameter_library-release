0a5009224dfcb9b8e0ac03121204a98d79a7c6e5
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2026-08-08 06:41:22 +0000

    Rebase from 'upstream'

--- example_python/setup.py
+++ example_python/setup.py
@@ -47,7 +47,7 @@ if len(sys.argv) >= 2 and sys.argv[1] != 'clean':
 
 setup(
     name=package_name,
-    version='0.7.5',
+    version='0.7.6',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
